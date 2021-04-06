c6c17379fa282cbfa9597d4bf10572c4fd02e1ca
Loi Truong <loitruong1989@gmail.com>
2021-04-06 11:32:03 -0400

    slider animation
    adding new slider animation -- avoid third party animation plugin
    reply on pure css to do all the animation
    
    adding banner 4 to test not using in production

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {createRef} from 'react';
+import ReactDOM from 'react-dom';
 // I might use this to do the component transition but will see about that 
 //import ReactCSSTransitionGroup from 'react-transition-group';
 import { ReactComponent as IconDown } from '../svg/icon-down.svg';
@@ -7,6 +8,19 @@ import { ReactComponent as IconLeft } from '../svg/icon-leftarrow.svg';
 import { ReactComponent as IconRight } from '../svg/icon-rightarrow.svg';
 import { ReactComponent as IconMouse } from '../svg/icon-mouse.svg';
 import './slider.scss';
+
+
+enum Direction {
+	NEXT,
+	PREVIOUS
+}
+
+enum CSSANIMATION {
+	CENTER2NEXT = "center2next",
+	CENTER2PREV = "center2prev",
+	PREV2CENTER = "prev2center",
+	NEXT2CENTER = "next2center"
+}
 
 export default class Slider extends React.Component<{},{slideName:string}> {
 
@@ -18,6 +32,18 @@ export default class Slider extends React.Component<{},{slideName:string}> {
 	currentWheel : number;
 	WHEELTRIGGER = 3;
 
+	//current Element
+	mRef 			= createRef<HTMLDivElement>();
+	mElement  : HTMLDivElement | null = null;
+
+	isChangingSlide : boolean;
+
+	//20210406: LTCM-LOOKBACK
+	// This is a total estimation animation time of a slide that needs to be swap;
+	// I need to come up with a better solution to calculate the time instead of reply on hard-code for this number
+	// the barrier is I'm using animation via CSS class, and it's hard to get the variable from SCSS
+	// maybe I should define CSS animation duration in javascript
+	slideTotalAnimationTime: number = 1000 
 
 
 	constructor(props:any){
@@ -27,6 +53,7 @@ export default class Slider extends React.Component<{},{slideName:string}> {
 			"banner-01.jpg",
 			"banner-02.jpg",
 			"banner-03.jpg",
+			"banner-04.jpg",
 		];
 
 		this.currentWheel = 0; 
@@ -35,68 +62,103 @@ export default class Slider extends React.Component<{},{slideName:string}> {
 
 		//set default state 
 		this.state = {slideName: this.banners[this.activeSlide] };
+
+		this.isChangingSlide = false;
 	}
 
 
-	changeSlide(){
-		this.setState({
-			slideName: this.banners[this.activeSlide] 
-		})
+	changeSlide(d:Direction){
+		//before changing state let's do something fun -- ANIMATION!!! wohoooo
+		//Since, i'm not sure what to use for the react animation library 
+		//I decided to use pure css	
+		let activeSlide = this.mElement!.querySelector(".Slider__Slide.Active");
+
+		if (activeSlide === null){
+			throw new Error("current slide not found. hmm this shouldn't happen.");
+			return;
+		}
+
+		let prevSlide = activeSlide!.previousElementSibling || this.mElement!.querySelector(".Slider__Slide:last-child");
+		let nextSlide = activeSlide!.nextElementSibling || this.mElement!.querySelector(".Slider__Slide:first-child");
+		let self = this;
+
+		switch(d){
+			case Direction.NEXT:
+				activeSlide!.classList.add(CSSANIMATION.CENTER2PREV);
+				nextSlide!.classList.add(CSSANIMATION.NEXT2CENTER);
+				break;
+			case Direction.PREVIOUS:
+				activeSlide!.classList.add(CSSANIMATION.CENTER2NEXT);
+				prevSlide!.classList.add(CSSANIMATION.PREV2CENTER);
+				break;
+		}
+
+
+		setTimeout(function() { 
+			self.setState({
+				slideName: self.banners[self.activeSlide] 
+			});
+
+			self.isChangingSlide = false;
+
+		}, self.slideTotalAnimationTime)//1000 is waiting for all animation to be done
+		
+		console.log(activeSlide, nextSlide, prevSlide);		
+		
 	}
 
 	handleWheel(event:any, self:Slider){
 		/*
-			Note From LoiT:
+			LTCM-NOTE
 			This slider will be base on wheel to be either change slider up/prev or down/next slide
 
 			{WHEELTRIGGER} - when user wheel to the same direction and reach to this number and it will trigger the event
 		*/
-		// event.preventDefault();
-		
-		console.log(event.deltaY, self.currentWheel);
+
+		if (self.isChangingSlide) return;
 
 
+		//this logic is to cancel the oppsoite wheel direction when it not yet to meet with WHEELTRIGGER logic
 		if ((event.deltaY > 0 && self.currentWheel < 0) 
 			|| (event.deltaY < 0 && self.currentWheel > 0)){
 			self.currentWheel = 0;
 		}
 
+
 		self.currentWheel += event.deltaY;
-
-
-
 		
 		if(self.currentWheel%self.WHEELTRIGGER === 0 && self.currentWheel > 0){
 			//Down/next slide
-			console.log('hey triggering down/next slide');
+			self.isChangingSlide = true;
 			self.activeSlide++;
 			if(self.activeSlide >= self.banners.length){
 				self.activeSlide = 0;
 			}
-			self.changeSlide();
+			self.changeSlide(Direction.NEXT);
 		}else if (self.currentWheel%self.WHEELTRIGGER === 0 && self.currentWheel < 0){
 			//up/Prev slide
+			self.isChangingSlide = true;
 			self.activeSlide--;
 			if(self.activeSlide < 0){
 				self.activeSlide = self.banners.length-1;
 			}
-			self.changeSlide();
+			self.changeSlide(Direction.PREVIOUS);
 
 		}
-
-
-
 	}
 
 
 	_handleWheelWrapper (event: any){
-		throw "_handleWheelWrapper need to be implement before use";		
+		throw new Error("_handleWheelWrapper need to be implement before use" );
 	} 
 
 	componentDidMount() {
 		var self = this;
 		
-		//Note from LoiT:
+		this.mElement = this.mRef.current;
+
+
+		//LTCM-LOOKBACK
 		//This is an ugly or only way that I can think of to be able to pass
 		//paramter into the event listener without using anonymous function
 		//I cant use anoynyumous function because I need to remove the event after componenent unmount
@@ -117,17 +179,21 @@ export default class Slider extends React.Component<{},{slideName:string}> {
 
 
   render() {
-  	console.log(this.state)
+  	var self = this;
     return (
-    	<div className="Slider">
-    		<Slide slideName={this.state.slideName} />
+    	<div ref={this.mRef} className="Slider">
+    		<div>
+					{this.banners.map((banner:string, index) => {
+	           return  <Slide isActive={self.activeSlide === index} slideName={banner} />
+					})}
+				</div>
     		<div className="Slider__Controller">
     			<button  type="button"><span>Up</span><IconUp /></button>
     			<span><IconMouse /></span>
     			<button  type="button"><span>Down</span><IconDown /></button>
     		</div>
 	    	<div className="Slider__MobileController">
-	    	  <button className="Slider__MobileController__Previous"  type="button"><span>Previous</span><IconLeft /></button>
+	    	  <button className="Slider__MobileController__Previous" type="button"><span>Previous</span><IconLeft /></button>
 	    	  <button className="Slider__MobileController__Next" type="button"><span>Next</span><IconRight /></button>
 	    	</div>
     	</div>
@@ -135,14 +201,15 @@ export default class Slider extends React.Component<{},{slideName:string}> {
   }
 }
 
-class Slide extends React.Component<{slideName:string},{}>{
-	bannerPath : string =  process.env.PUBLIC_URL +"/assets/banner/";
+class Slide extends React.Component<{slideName:string, isActive:boolean},{}>{
+	BANNERPATH : string =  process.env.PUBLIC_URL +"/assets/banner/";
 	render(){
 		let slideStyle = {
-			backgroundImage: "url("+ this.bannerPath + this.props.slideName + ")"
+			backgroundImage: "url("+ this.BANNERPATH + this.props.slideName + ")"
 		}
+		let activeClass = this.props.isActive ? 'Active' : '';
 		return (
-			<div className="Slider__Slide Active">
+			<div className={`Slider__Slide ${activeClass}`}>
 				<div className="Slider__SlideBG" style={slideStyle}>
 				</div>
 			</div>
